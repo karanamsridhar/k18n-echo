59bd90665c5965fe5b9844f0ef580e01bed9e7df
karanamsridhar <karanamsridhar@gmail.com>
2022-06-25 08:50:12 -0500

    Initial commit

---
+++ src/test/java/com/k18n/app/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k18n.app.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
